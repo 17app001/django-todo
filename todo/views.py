e3aa9606e4b5e687987b65fcec10bb0854d4de69
Troie <troiepan@gmail.com>
2024-09-29 10:50:53 +0800

    完成首頁安排

--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,16 @@ from .models import Todo
 
 
 # Create your views here.
-def todolist(request):
-    todos = Todo.objects.all()
+def todolist(request):    
+    message=""
+    user=request.user
+    todos=None
+    if user.is_authenticated:
+        todos = Todo.objects.filter(user=request.user)
+        if not todos:
+            message="請先新增代辦事項"
+    else:   
+        message="請先登入或註冊"
+ 
     print(todos)
-    return render(request, "todo/todolist.html", {"todos": todos})
+    return render(request, "todo/todolist.html", {"todos": todos,"message":message})
